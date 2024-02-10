6ae65f7297a9230ce8d5e93932a2d21279ca3515
Sebastian Gomez <gomezsebitas123@gmail.com>
2024-02-10 11:23:52 -0500

    Agrego Ejercicio 2

---
+++ Ejercicio2(SumarDosNumeros).py
@@ -0,0 +1,6 @@
+print('suma de dos numeros')
+a = int(input('ingrese el valore de a:'))
+b = int(input('ingrese el valor de b:'))
+resultadoSuma = a + b
+print('el resultado es:')
+print(resultadoSuma)
